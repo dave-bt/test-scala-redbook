d35389aa026dee81330825d505697085653717f0
dave.beattie <dave.beattie@tradition.com>
2018-11-23 16:57:12 +0000

    checking compiles

---
+++ src/main/errorhandling/MyOption.scala
@@ -0,0 +1,17 @@
+package main.errorhandling
+
+case class Pear(core : Int, descr: String) {
+
+  def GetDescr : String = {
+    descr + " " + core
+  }
+
+}
+
+object Controller {
+  def main (args: Array[String] ): Unit = {
+
+  }
+}
+
+
